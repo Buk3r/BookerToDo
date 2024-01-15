bc7771371f2ccc36865e8c060b4515e7e54b0ad0
Buk3r <eugene.booker99@gmail.com>
2024-01-15 21:26:46 +0200

    Add project files.

---
+++ BookerToDo/BookerToDo/Views/AddEditTask.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BookerToDo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddEditTask : ContentPage
+    {
+        public AddEditTask()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BookerToDo/BookerToDo/Views/MainListPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BookerToDo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainListPage : ContentPage
+    {
+        public MainListPage()
+        {
+            InitializeComponent();
+
+            todoList.ItemsSource = new List<string>
+            {
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+                "string",
+            };
+        }
+
+        private void OnButtonClicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new AddEditTask());
+        }
+    }
+}
